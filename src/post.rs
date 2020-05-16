33adab1a6b9047743746a745b257d7603b12a130
Lonami Exo <totufals@hotmail.com>
2020-05-16 11:30:29 +0200

    Proper post metadata parsing

--- src/post.rs
+++ src/post.rs
@@ -1,12 +1,11 @@
 use crate::html;
 use crate::FOLDER_POST_NAME;
 
-use pulldown_cmark::{Event, Parser, Tag};
-use std::collections::HashMap;
-use std::collections::VecDeque;
+use pulldown_cmark::{CodeBlockKind, Event, Parser, Tag};
 use std::error::Error;
 use std::ffi::{OsStr, OsString};
 use std::fs;
+use std::ops::Range;
 use std::path::{Path, PathBuf};
 
 use chrono::offset::Local;
@@ -32,28 +31,85 @@ struct Metadata {
 }
 
 impl Metadata {
-    fn update_from_kvp(&mut self, kvp: &HashMap<String, String>) {
-        for (key, value) in kvp.iter() {
-            match key.to_lowercase().as_ref() {
-                "title" => self.title = Some(value.trim().to_string()),
-                "path" => self.path = value.trim().into(),
-                "created" => {
-                    self.created = parse_date(value)
-                        .expect(&format!("Invalid `created` override \"{}\"", value));
-                }
-                "modified" => {
-                    self.modified = parse_date(value)
-                        .expect(&format!("Invalid `modified` override \"{}\"", value));
-                }
-                _ => {
-                    eprintln!(
-                        "Unexpected meta override key \"{}\" in post {}, ignoring.",
-                        key,
-                        self.path.to_string_lossy()
-                    );
-                }
-            }
+    fn update_from_contents(&mut self, contents: &str) -> Option<Range<usize>> {
+        #[derive(Debug)]
+        enum State {
+            Idle,
+            WaitTitle,
+            WaitMeta(usize),
+            WaitEndMeta(usize),
         }
+
+        let mut remove_range = None;
+
+        Parser::new(contents)
+            .into_offset_iter()
+            .fold(State::Idle, |state, (event, range)| match event {
+                Event::Start(Tag::Heading(1)) => State::WaitTitle,
+                Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(lang)))
+                    if lang.as_ref() == "meta" =>
+                {
+                    State::WaitMeta(range.start)
+                }
+                Event::Text(string) => match state {
+                    State::WaitTitle if self.title.is_none() => {
+                        self.title = Some(string.to_string());
+                        State::Idle
+                    }
+                    State::WaitMeta(range_start) => {
+                        self.update_from_meta_contents(&string);
+                        State::WaitEndMeta(range_start)
+                    }
+                    _ => State::Idle,
+                },
+                Event::End(Tag::CodeBlock(CodeBlockKind::Fenced(lang)))
+                    if lang.as_ref() == "meta" =>
+                {
+                    match state {
+                        State::WaitEndMeta(range_start) => {
+                            remove_range = Some(range_start..range.end);
+                            State::Idle
+                        }
+                        _ => panic!(format!("Invalid state reached {:?}", state)),
+                    }
+                }
+                _ => state,
+            });
+
+        remove_range
+    }
+
+    fn update_from_meta_contents(&mut self, contents: &str) {
+        contents
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .for_each(|line| {
+                let mut kv = line.splitn(2, ':');
+                let key = kv.next().unwrap();
+                let value = kv
+                    .next()
+                    .expect(&format!("Meta line \"{}\" should contain a colon", line));
+
+                match key.to_lowercase().as_ref() {
+                    "title" => self.title = Some(value.trim().to_string()),
+                    "path" => self.path = value.trim().into(),
+                    "created" => {
+                        self.created = parse_date(value)
+                            .expect(&format!("Invalid `created` override \"{}\"", value));
+                    }
+                    "modified" => {
+                        self.modified = parse_date(value)
+                            .expect(&format!("Invalid `modified` override \"{}\"", value));
+                    }
+                    _ => {
+                        eprintln!(
+                            "Unexpected meta override key \"{}\" in post {}, ignoring.",
+                            key,
+                            self.path.to_string_lossy()
+                        );
+                    }
+                }
+            });
     }
 }
 
@@ -106,59 +162,13 @@ impl Post {
     }
 
     /// Partially parses markdown to apply meta overrides
-    fn from_sources(markdown: String, assets: Vec<PathBuf>, mut meta: Metadata) -> Self {
-        let mut wait_title = false;
-        for event in Parser::new(&markdown) {
-            match event {
-                Event::Start(Tag::Heading(1)) => wait_title = true,
-                Event::Text(string) if wait_title => {
-                    meta.title = Some(string.to_string());
-                    break;
-                }
-                _ => {}
-            }
+    fn from_sources(mut markdown: String, assets: Vec<PathBuf>, mut meta: Metadata) -> Self {
+        if let Some(remove_range) = meta.update_from_contents(&markdown) {
+            markdown.replace_range(remove_range, "");
         }
-
-        // Parse meta overrides
-        let mut meta_kvp: HashMap<String, String> = HashMap::new();
-        let mut lines: VecDeque<&str> = markdown.split('\n').collect();
-
-        if lines[0] == "```meta" {
-            lines.pop_front();
-            loop {
-                let line = lines
-                    .pop_front()
-                    .expect("Unexpected EOF while parsing meta");
-                if line == "```" {
-                    break;
-                }
-                let mut kv = line.splitn(2, ':');
-                let key = kv.next().unwrap();
-                let value = kv
-                    .next()
-                    .expect(&format!("Meta line \"{}\" should contain a colon", line));
-                meta_kvp.insert(key.into(), value.into());
-            }
-        }
-
-        // Store everything after the meta info on the Post. Re-join with newlines.
-        let content = {
-            let mut result = String::new();
-            let mut iter = lines.into_iter();
-            if let Some(line) = iter.next() {
-                result.push_str(line);
-            }
-            for line in iter {
-                result.push('\n');
-                result.push_str(line);
-            }
-            result
-        };
-
-        meta.update_from_kvp(&meta_kvp);
 
         Post {
-            markdown: content,
+            markdown,
             path: meta.path,
             title: meta.title.unwrap_or_else(|| "(no title)".to_string()),
             modified: meta.modified,
@@ -235,7 +245,7 @@ Some words.";
         assert_eq!(
             post.markdown,
             concat!(
-                "# My blog post with a long title to be overridden\n\n",
+                "\n# My blog post with a long title to be overridden\n\n",
                 "Some words."
             )
         );

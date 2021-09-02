04de4386dac657b35fb95111fb757eb8ed0c21e8
Lonami Exo <totufals@hotmail.com>
2021-09-02 17:12:37 +0200

    Restructure main in preparation for new rules

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,59 @@
-mod blog;
-mod escape;
-mod fs_action;
-mod html;
-mod post;
-
+use std::env;
+use std::io;
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 
-use anyhow::{anyhow, Context, Result};
+pub const SOURCE_PATH: &str = "content";
+pub const TARGET_PATH: &str = "dist";
 
-pub use blog::Blog;
-pub use post::Post;
+/// Process the non-directory file `src` and put its result in `dst`.
+fn process(src: &PathBuf, dst: &mut PathBuf) -> io::Result<()> {
+    dst.push(src.file_name().expect("file had no name"));
+    // TODO process different files differently
+    fs::copy(src, &*dst)?;
+    dst.pop();
+    Ok(())
+}
 
-pub const DEFAULT_CONTENT_PATH: &str = "content";
-pub const HEADER_FILE_NAME: &str = "header.md";
-pub const FOOTER_FILE_NAME: &str = "footer.md";
-pub const FOLDER_POST_NAME: &str = "post.md";
-pub const CSS_FILE_NAME: &str = "style.css";
-pub const CSS_DIR_NAME: &str = "css";
-
-fn main() -> Result<()> {
-    let blog = Blog::from_source_dir(DEFAULT_CONTENT_PATH)?;
-
-    let output_dir = Path::new("dist");
-    if !output_dir.exists() {
-        eprintln!(
-            "Creating output directory \"{}\"...",
-            output_dir.to_string_lossy()
-        );
-        fs::create_dir(output_dir).context("Could not create output directory")?;
+/// Walks `src`, saving the result to `dst`.
+///
+/// `src` and `dst` may contain different values if this function exits with `Err`.
+fn walk(src: &mut PathBuf, dst: &mut PathBuf) -> io::Result<()> {
+    if !dst.is_dir() {
+        fs::create_dir(&*dst)?;
     }
 
-    // TODO consider using FsAction here for DRY
-    if !output_dir.is_dir() {
-        return Err(anyhow!(
-            "Could not create output directory: path '{:?}' already exists",
-            output_dir
-        ));
+    for entry in fs::read_dir(&*src)? {
+        let entry = entry?;
+        src.push(entry.file_name());
+
+        if src.is_dir() {
+            dst.push(entry.file_name());
+            walk(src, dst)?;
+            dst.pop();
+        } else {
+            process(src, dst)?;
+        }
+
+        src.pop();
     }
 
-    blog.generate(output_dir)?;
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let root = match env::args().nth(1) {
+        Some(path) => path.into(),
+        None => env::current_dir()?,
+    };
+
+    let mut content = root.clone();
+    content.push(SOURCE_PATH);
+
+    let mut dist = root;
+    dist.push(TARGET_PATH);
+
+    walk(&mut content, &mut dist)?;
 
     Ok(())
 }
